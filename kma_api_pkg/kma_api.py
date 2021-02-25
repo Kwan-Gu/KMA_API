855401ec5d79128923518c606adcbee0260b34da
Kwan-Gu, Baek <bouguereau1130@gmail.com>
2021-02-25 10:12:10 +0900

    Update kma_api_pkg/kma_api 주석 추가

--- kma_api_pkg/kma_api.py
+++ kma_api_pkg/kma_api.py
@@ -1,12 +1,45 @@
+# IMPORT: 필요한 패키지 및 모듈
 import json
 import pandas as pd
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
 
+# SET: API 서비스 인증키 발급, API CALL URL 설정
 key = open('kma_api_pkg/key_백관구.txt').read()
 url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
 
 class load:
+    """
+    Description
+    ================================================================
+    기상청 예보 API로부터 데이터를 수집하여 DataFrame 형태로 출력합니다.
+
+    Parameters
+    ================================================================
+    base_date: str
+        예보 시점 날짜('20210225').
+    
+    base_time: str
+        예보 시점 시각('0800').
+    
+    nx: str
+        예보 위치의 x 좌표('62').
+        
+    ny: str
+        예보 위치의 y 좌표('121').
+    
+    key: str (default = key)
+        API 서비스 인증키('93N09EBDBc23FY%2FFm').
+    
+    url: str (default = url)
+        API Call URL 주소('http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst').
+    
+
+    Methods
+    ================================================================
+    run()
+        DataFrame 형태의 기상청 예보 데이터 출력
+    """
     def __init__(self, base_date, base_time, nx, ny, key = key, url = url):
         self.base_date = base_date
         self.base_time = base_time
