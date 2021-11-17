9c2b6bc76e3df41da72bec8632baf9ba9000f8f3
Kwan-Gu <bouguereau1130@gmail.com>
2021-11-17 15:09:50 +0900

    기상청 ASOS 조회 서비스 추가

--- kma_api_pkg/kma_api.py
+++ kma_api_pkg/kma_api.py
@@ -4,11 +4,81 @@ import pandas as pd
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
 
-# SET: API 서비스 인증키 발급, API CALL URL 설정
-key = open('kma_api_pkg/key_백관구.txt').read()
-url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
+# SET: API 서비스 인증키 발급
+key = open('key_백관구.txt').read()
 
-class load:
+class ASOS:
+    """
+    Description
+    ================================================================
+    기상청 ASOS API로부터 데이터를 수집하여 DataFrame 형태로 출력합니다.
+
+    Parameters
+    ================================================================
+    startDt: str
+        조회 기간 시작일('20211116').
+    
+    startHh: str
+        조회 기간 시작시('00').
+    
+    endDt: str
+        조회 기간 종료일('20211116').
+        
+    endHh: str
+        조회 기간 종료시('23').
+    
+    stnIds: str
+        종관기상관측 지점 번호('108')
+
+    key: str (default = key)
+        API 서비스 인증키('Encode').
+    
+
+    Methods
+    ================================================================
+    run()
+        DataFrame 형태의 기상청 예보 데이터 출력
+    """
+    def __init__(self, startDt, startHh, endDt, endHh, stnIds, key = key):
+        self.startDt = startDt
+        self.startHh = startHh
+        self.endDt   = endDt
+        self.endHh   = endHh
+        self.stnIds  = stnIds
+        self.key     = key
+        self.url     = 'http://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList'
+    
+    def _get_json(self):
+        query = f"?{quote_plus('ServiceKey')}={self.key}&" +\
+         urlencode({quote_plus('pageNo')    : '1',\
+					quote_plus('numOfRows') : '24',\
+					quote_plus('dataType')  : 'JSON',\
+                    quote_plus('dataCd')    : 'ASOS',\
+                    quote_plus('dateCd')    : 'HR',\
+					quote_plus('startDt')   : str(self.startDt),\
+					quote_plus('startHh')   : str(self.startHh),\
+					quote_plus('endDt')     : str(self.endDt),\
+					quote_plus('endHh')     : str(self.endHh),\
+                    quote_plus('stnIds')    : str(self.stnIds)})
+        request = Request(self.url + query)
+        request.get_method = lambda: 'GET'
+        content = urlopen(request).read().decode('utf-8')
+        return content
+    
+    def _to_df(self, json_content):
+        json_content = json.loads(json_content)['response']
+        header = json_content['header']
+        if header['resultMsg'] == 'NORMAL_SERVICE': pass
+        else: raise InterruptedError(f"resultCode: {header['resultCode']}, resultMsg: {header['resultMsg']}")
+        df_content = pd.DataFrame(json_content['body']['items']['item'])
+        return df_content
+
+    def run(self):
+        return self._to_df(self._get_json())
+
+
+# 서비스 종료됨(단기예보로 변경해야 함)
+class Forecast:
     """
     Description
     ================================================================
@@ -31,22 +101,19 @@ class load:
     key: str (default = key)
         API 서비스 인증키('93N09EBDBc23FY%2FFm').
     
-    url: str (default = url)
-        API Call URL 주소('http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst').
-    
 
     Methods
     ================================================================
     run()
         DataFrame 형태의 기상청 예보 데이터 출력
     """
-    def __init__(self, base_date, base_time, nx, ny, key = key, url = url):
+    def __init__(self, base_date, base_time, nx, ny, key = key):
         self.base_date = base_date
         self.base_time = base_time
         self.nx        = nx
         self.ny        = ny
         self.key       = key
-        self.url       = url
+        self.url       = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
     
     def _get_json(self):
         query = f"?{quote_plus('ServiceKey')}={self.key}&" +\
